3eba27f4758be03fb96262a6c5fc6f8e2f0ba727
RKovl <ryan.kovlaske@gmail.com>
2022-10-13 11:48:04 -0700

    footer layoput done

--- frontEnd/src/components/layout/BasicLayout.tsx
+++ frontEnd/src/components/layout/BasicLayout.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import SeachHeader from './SeachHeader';
 import SubHeader from './SubHeader';
+import Footer from './Footer';
 
 const BasicLayout = (props: any) => {
   return (
-    <div className=' px-4 bg-gray-800'>
+    <div className='  bg-gray-800'>
 
-        <div className='container mx-auto'>
+        <div className='container mx-auto px-4'>
 
             <SeachHeader/>
             <SubHeader/>
             {props.children}
+            
 
         </div>
+
+        <Footer/>
 
     </div>
   )
