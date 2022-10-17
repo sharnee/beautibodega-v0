c7d91886eb40c3a079d7388a829e0ab20adaa2a4
RKovl <ryan.kovlaske@gmail.com>
2022-10-17 07:00:38 -0700

    updated skeleton

--- frontEnd/src/components/Profile/EditProfile.tsx
+++ frontEnd/src/components/Profile/EditProfile.tsx
@@ -13,6 +13,9 @@ const EditProfile = () => {
         <form>
             <div className="grid gap-6 mb-6 md:grid-cols-2">
                 <div>
+                    <div className="rounded-full p-5 m-3 text-center bg-gray-300">round this, this is for the profile image</div>
+                </div>
+                <div>
                     <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Name</label>
                     <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" />
                 </div>
