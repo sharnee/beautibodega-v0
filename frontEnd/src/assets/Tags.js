f98fce626942d77301d3820160fa98250f3da3b7
RKovl <ryan.kovlaske@gmail.com>
2022-10-18 14:10:03 -0700

    added tags and types

---
+++ frontEnd/src/assets/Tags.js
@@ -0,0 +1,32 @@
+let Tags = {
+    worldTags: [
+        "African American or African Diaspora", 
+        "Hispanic or Latinx", 
+        "Asian American",
+        "Multiracial", 
+        "Indigenous Peoples",
+        "South Asian Americans",
+        "LGBTQ+"
+    ],
+    forTags:[
+        "Acne",
+        "Acne Scars", 
+        "Aging skin", 
+        "Black Heads", 
+        "Dark Spots", 
+        "Rosacea", 
+        "Oil Control", 
+        "Irritated Skin", 
+        "Large Pores", 
+        "Stretch Marks", 
+        "Wrinkles", 
+        "Hyperpigmentation",
+        "Acne-Prone Skin", 
+        "Sensitive Skin", 
+        "Dry Skin", 
+        "Mature Skin", 
+        "Combination Skin", 
+        "Oily Skin",
+        "Normal Skin" 
+    ]
+}
